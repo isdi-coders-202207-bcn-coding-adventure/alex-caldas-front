ed4b52ad21074e71b127bac4f2b752b56c6901a1
Alex Caldas <alexcaldasaixarch@gmail.com>
2022-08-22 16:53:23 +0200

    fixed nesting selectors on TimerStyled

--- src/components/Timer/TimerStyled.js
+++ src/components/Timer/TimerStyled.js
@@ -9,25 +9,27 @@ const TimerStyled = styled.ul`
   flex-direction: row;
   gap: 15px;
 
-  .timer__component {
-    display: flex;
-    flex-direction: column;
-  }
-  .timer__data {
-    font-weight: 600;
-    font-size: 1.7rem;
-    width: 4rem;
-    height: 4rem;
-    border-radius: 1rem;
-    color: #243342;
-    background-color: #ffc700;
-    display: grid;
-    place-items: center;
-  }
-  .timer__name {
-    font-weight: 600;
-    color: #b6d3e6;
-    text-align: center;
+  .timer {
+    &__component {
+      display: flex;
+      flex-direction: column;
+    }
+    &__data {
+      font-weight: 600;
+      font-size: 1.7rem;
+      width: 4rem;
+      height: 4rem;
+      border-radius: 1rem;
+      color: #243342;
+      background-color: #ffc700;
+      display: grid;
+      place-items: center;
+    }
+    &__name {
+      font-weight: 600;
+      color: #b6d3e6;
+      text-align: center;
+    }
   }
 `;
 
