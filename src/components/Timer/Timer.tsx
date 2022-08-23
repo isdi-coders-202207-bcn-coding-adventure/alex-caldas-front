4fa8d21d1dfc9e9146dd03832571f8f066db5c51
Alex Caldas <alexcaldasaixarch@gmail.com>
2022-08-23 13:41:53 +0200

    adde funcitonality to Timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
 import TimerStyled from "./TimerStyled";
 
 const Timer = (): JSX.Element => {
+  interface Time {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
+
+  const initialState: Time = {
+    days: 31,
+    hours: 24,
+    minutes: 60,
+    seconds: 60,
+  };
+
+  const [state, setState] = useState(initialState);
+
+  setInterval(() => {
+    const date = new Date();
+    const newDate: Time = {
+      days: initialState.days - date.getDay(),
+      hours: initialState.hours - date.getHours(),
+      minutes: initialState.minutes - date.getMinutes(),
+      seconds: initialState.seconds - date.getSeconds(),
+    };
+    setState(newDate);
+  }, 1000);
+
   return (
     <TimerStyled>
       <li className="timer__component">
-        <span className="timer__data">02</span>
+        <span className="timer__data">{state.days}</span>
         <span className="timer__name">days</span>
       </li>
       <li className="timer__component">
-        <span className="timer__data">20</span>
+        <span className="timer__data">{state.hours}</span>
         <span className="timer__name">hours</span>
       </li>
       <li className="timer__component">
-        <span className="timer__data">34</span>
+        <span className="timer__data">{state.minutes}</span>
         <span className="timer__name">minutes</span>
       </li>
       <li className="timer__component">
-        <span className="timer__data">09</span>
-        <span className="timer__name">second</span>
+        <span className="timer__data">{state.seconds}</span>
+        <span className="timer__name">seconds</span>
       </li>
     </TimerStyled>
   );
